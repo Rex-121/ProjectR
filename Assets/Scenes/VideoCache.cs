bab7fe623eb301b5c8938ebfbfb5acd71ae1104c
Tyrant <>
2021-09-25 17:49:52 +0800

    增加UI

---
+++ Assets/Scenes/VideoCache.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using RenderHeads.Media.AVProVideo;
+
+public class VideoCache : MonoBehaviour
+{
+
+    [SerializeField] MediaPlayer _mediaPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (_mediaPlayer.Cache.IsMediaCachingSupported())
+        {
+            Debug.Log("-------support");
+            _mediaPlayer.Cache.AddMediaToCache("https://roobo-test.oss-cn-beijing.aliyuncs.com/appcourse/video/2021-09-14/c5068d53hct220o5lq10.mp4_PKG/Video.m3u8", null, null);
+        }
+        else
+        {
+            Debug.Log("-------not-support");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
