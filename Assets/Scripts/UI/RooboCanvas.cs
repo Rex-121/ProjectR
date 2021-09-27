c1f258a83770929c46d4ae424445682d0088a75c
Tyrant <>
2021-09-27 17:12:13 +0800

    整理文件夹

---
+++ Assets/Scripts/UI/RooboCanvas.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RooboCanvas : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void Back() {
+        Debug.Log("按下Back按钮");
+    }
+
+}
